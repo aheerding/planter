088140eada91e0f5ed1e5c1226c195e8377b724a
Alex Heerding <aheerding@gmail.com>
2020-08-23 22:04:51 -0400

    Initial setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import { auth } from './firebase'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+let app
+
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      render: (h) => h(App)
+    }).$mount('App')
+  }
+})
